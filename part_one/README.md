9f0af188b3a3978be6f7a0efe922f6ce47e493f8
Rahul T Gerorge <34202955+rahul-george@users.noreply.github.com>
2025-01-24 10:12:34 +0000

    Basic example created without the open telemetry

---
+++ part_one/README.md
@@ -0,0 +1,2 @@
+# Problem
+We are going to create the a simple CLI application and hook the open telemetry SDK and APIs on this. 
---
+++ part_one/app.py
@@ -0,0 +1,7 @@
+def add(a: int, b: int):
+    return a+b
+
+
+if __name__ == "__main__":
+    return_val = add(5, 6)
+    print(return_val)
