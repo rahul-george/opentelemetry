4cd00d620616083f1ae375243ed329723942fec6
Rahul T Gerorge <34202955+rahul-george@users.noreply.github.com>
2025-01-24 10:20:39 +0000

    Two functions with decorators on how to trace.

--- part_one/app.py
+++ part_one/app.py
@@ -1,5 +1,27 @@
-def add(a: int, b: int):
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter
+)
+
+provider = TracerProvider()     
+processor = BatchSpanProcessor(ConsoleSpanExporter())       # How spans are processed. Processed in batches and exported onto the console. 
+provider.add_span_processor(processor)
+
+
+# Sets the global default tracer provider
+trace.set_tracer_provider(provider)
+
+tracer = trace.get_tracer("my.tracer.name")
+
+@tracer.start_as_current_span("_add")
+def _add(a: int, b: int):
     return a+b
+
+@tracer.start_as_current_span("add")
+def add(a: int, b: int):
+    return _add(a, b)
 
 
 if __name__ == "__main__":
